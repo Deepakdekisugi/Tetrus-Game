9f883d3f6cc13abe4723801588fd2ebc9e92fc0f
Deepak <55588916+Deepakdekisugi@users.noreply.github.com>
2023-06-11 14:52:35 +0530

    added player drop and rotate function

--- script.js
+++ script.js
@@ -121,3 +121,28 @@ function merge(player, arena){
     });
 }
 
+function rotate(matrix, dir){
+    for(let y = 0; y < matrix.length; ++y){
+        for(let x = 0; x < y; ++x){
+            [matrix[x, y],matrix[y, x] = matrix[y, x], matrix[x, y]];
+        }
+    }
+    if(dir > 0){
+        matrix.forEach((row) => row.reverse());
+    }else{
+        matrix.reverse();
+    }
+}
+
+function playerDrop(){
+
+    player.pos.y++;
+    if(collide(arena, player)){
+        player.pos.y--;
+        merge(arena, player);
+        playerReset();
+        arenaSweep();
+        updateScore();
+    }
+    dropCounter = 0;
+}
